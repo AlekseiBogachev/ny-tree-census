1a808f7bffca48cf35068d2e3b5ea9203a6f59bc
Aleksei Bogachev <bogachev.aleksey.m@gmail.com>
2024-04-26 05:13:28 +0000

    Начата функция, загружающая датасет. Написана функция выполняющая запрос к SODA API и возвращающая обработанный json

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -3,6 +3,7 @@
 
 import logging
 from pathlib import Path
+from typing import Any, Dict, List, Optional
 
 import click
 import requests
@@ -78,6 +79,69 @@ def get_description(output_path: str, chunk_size: int) -> None:
     return None
 
 
+def request_soda_json(
+    endpoint: str, params: Dict[str, str], timeout: int = 10
+) -> Optional[List[Dict[str, Any]]]:
+    """Выполняет запрос к SODA API.
+
+    Выполняет запрос к SODA API с конечной точкой endpoint и параметрами params,
+    и получает в ответ json.
+    См. https://dev.socrata.com/foundry/data.cityofnewyork.us/uvpi-gqnh .
+
+    Parameters
+    ----------
+    endpoint : str
+        Конечная тока - адрес, по которому будет оправлен запрос.
+    timeout: int, optional
+        Время ожидания ответа от сервера в секундах, после которого будет
+        вызвана ошибка. Значение по умолчанию 10.
+    params : Dict[str, str]
+        Параметры запроса.
+
+    Returns
+    -------
+    Optional[List[Dict[str, Any]]]
+        Ответ SODA API. Если ответ не был получен или запрос завершился ошибкой,
+        то возвращается None. Если ответ был получен и успешно обработан, то
+        возвращается обработанный json - список из словарей параметров и
+        значений.
+    """
+    resp_json: Optional[List[Dict[str, Any]]] = None
+    try:
+        response: requests.Response = requests.get(
+            endpoint,
+            params=params,
+            timeout=timeout,
+        )
+
+        response.raise_for_status()
+
+        logger.info(
+            "Отправка запроса. "
+            f"url: {response.url} ; "
+            f"status code: {response.status_code} ; "
+            f"encoding: {response.encoding} ; "
+        )
+
+        resp_json = response.json()
+
+    except requests.exceptions.Timeout:
+        logger.error("Timeout!", exc_info=True)
+
+    except requests.exceptions.HTTPError:
+        logger.error("HTTPError!", exc_info=True)
+
+    except requests.exceptions.JSONDecodeError:
+        logger.error("JSONDecodeError!", exc_info=True)
+    else:
+        logger.info(
+            "Запрос успешно вернул ответ."
+            f"Длина ответа {len(response.content)} Байт."
+        )
+
+    return resp_json
+
+
 @click.command()
 @click.option(
     "-o",
@@ -100,7 +164,30 @@ def get_description(output_path: str, chunk_size: int) -> None:
     show_default=True,
     type=int,
 )
-def get_data(output_path: str, chunk_size: int = 1000) -> None:
+@click.option(
+    "-m",
+    "--maxrows",
+    "maxrows",
+    default=-1,
+    help="Общее количество загруженных строк. Если -1,"
+    "то загрузить весь датасет.",
+    show_default=True,
+    type=int,
+)
+@click.option(
+    "-t",
+    "--timeout",
+    "timeout",
+    default=10,
+    help="Время ожидания ответа от сервера в секундах, после которого будет"
+    "вызвана ошибка.",
+)
+def get_data(
+    output_path: str,
+    chunk_size: int = 1000,
+    maxrows: int = -1,
+    timeout: int = 10,
+) -> int:
     """Загружает датасет в формате .csv через SODA API.
 
     Загружает данные в формате csv с помощью SODA API
@@ -117,12 +204,30 @@ def get_data(output_path: str, chunk_size: int = 1000) -> None:
         Каталог, в которой будет сохранён файл с исходными данными data.csv .
         Значение по умолчанию <project dir>/data/raw/ .
     chunk_size : int, optional
-        Количество строк загружаемых за 1 запрос. API поставщика данных "
-        "ограничивает количество строк 1000." Значение по умолчанию 1000.
-    """
-    print("Загрузка исходных данных")
+        Количество строк загружаемых за 1 запрос. API поставщика данных
+        ограничивает количество строк 1000." Значение по умолчанию 1000.
+    nrows : int, optional
+        Общее количество загруженных строк. Если -1, то загружает весь датасет.
+        Значение по умолчанию -1.
+    timeout: int, optional
+        Время ожидания ответа от сервера в секундах, после которого будет
+        вызвана ошибка. Значение по умолчанию 10.
 
-    return None
+    Returns
+    -------
+    int
+        Количество полученных строк.
+    """
+    endpoint: str = "https://data.cityofnewyork.us/resource/uvpi-gqnh.json"
+
+    logger.info("Загрузка датасета через SODA API.")
+    payload: Dict[str, str] = {"$select": "count(*)"}
+
+    print(request_soda_json(endpoint, params=payload))
+
+    nrows_resp: int = 0
+
+    return nrows_resp
 
 
 cli.add_command(get_description)
