90acfdd29671f6afc5a952fb30268ccdb86f9a25
Aleksei Bogachev <bogachev.aleksey.m@gmail.com>
2024-05-09 13:13:20 +0000

    Install plotly

--- code-server.sh
+++ code-server.sh
@@ -5,7 +5,6 @@
 docker run \
 --detach \
 --name ny_tree_census_code_server \
---rm \
 -p 127.0.0.1:8080:8080 \
 ny_tree_census_dev \
 code-server \
@@ -18,5 +17,3 @@ echo "code-server is running"
 echo "code-server parameters:"
 docker exec -it ny_tree_census_code_server \
 cat /dockeruser/.config/code-server/config.yaml
-
-docker attach ny_tree_census_code_server
