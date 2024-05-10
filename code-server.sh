c1df6dfd24e8cc860cacb1b7571f2649c4c2aeed
Aleksei Bogachev <bogachev.aleksey.m@gmail.com>
2024-05-10 02:59:40 +0000

    Файл code-server.sh возвращён к версии для локального запуска, то есть версии, где в конце управление возвращается контейнеру

--- code-server.sh
+++ code-server.sh
@@ -5,6 +5,7 @@
 docker run \
 --detach \
 --name ny_tree_census_code_server \
+--rm \
 -p 127.0.0.1:8080:8080 \
 ny_tree_census_dev \
 code-server \
@@ -17,3 +18,5 @@ echo "code-server is running"
 echo "code-server parameters:"
 docker exec -it ny_tree_census_code_server \
 cat /dockeruser/.config/code-server/config.yaml
+
+docker attach ny_tree_census_code_server
